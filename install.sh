18ed8d5c2b69b3fe6e506a6eeb1d8021a4d3f13d
Angel-HF <49835792+Pelochus@users.noreply.github.com>
2024-04-12 23:18:17 +0200

    Moved package installs with apt to ezrknpu repo

--- install.sh
+++ install.sh
@@ -18,16 +18,10 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-message_print "Installing apt dependencies..."
-
-sudo apt update
-sudo apt install -y git git-lfs python-is-python3 python3-pip libxslt1-dev zlib1g-dev libglib2.0-dev libsm6 libgl1-mesa-glx libprotobuf-dev build-essential
-# sudo apt install -y adb # For running the NPU in Android
-
-message_print "Changing to repository..."
+# message_print "Changing to repository..."
 
 # git clone https://github.com/Pelochus/ezrknn-toolkit2.git
-cd ezrknn-toolkit2/
+# cd ezrknn-toolkit2/
 
 message_print "Installing pip dependencies for ARM64..."
 
